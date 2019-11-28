56fcb9d5870c51f7209a742847045ebeadd321db
Alexander <alexandervvugt@gmail.com>
2019-11-28 16:37:28 +0100

    added tests for Boat

--- gui/logic/BoatTest.py
+++ gui/logic/BoatTest.py
@@ -3,14 +3,25 @@ import Boat as Boat
 
 class BoatTest(unittest.TestCase):
     def load_negative(self):
-        self.assertEqual(boat.load(-5), True)
+        self.assertTrue(boat.load(-5), True)
         self.assertEqual(boat.getLoad(), 0)
         reset()
 
     def load_zero(self):
-        self.assertEqual(boat.load(0), True)
+        self.assertTrue(boat.load(0), True)
         self.assertEqual(boat.getLoad(), 5)
         reset()
+
+    def load_succes(self):
+        self.assertTrue(boat.load(5), True)
+        self.assertEqual(boat.getLoad(), 10)
+        reset()
+
+    def load_overload(self):
+        self.assertFalse(boat.load(6), False)
+        self.assertEqual(boat.getLoad(), 5)
+        reset()
+    
 
 def setup():
     """Sets up the test case"""
