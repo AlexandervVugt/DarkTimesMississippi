4b3be70ce98942aaf11be42c839794985ea282d4
Alexander <alexandervvugt@gmail.com>
2020-01-05 13:53:06 +0100

    implemented turn test

--- gui/logic/test_Turn.py
+++ gui/logic/test_Turn.py
@@ -5,5 +5,12 @@ class TurnTest(unittest.TestCase):
     def setUp(self):
         self.turn = Turn.Turn()
 
+    def test_initiation(self):
+        self.assertEqual(self.turn.getSteps(), -1)
+
+    def test_setsteps(self):
+        self.turn.setSteps(5)
+        self.assertEqual(self.turn.getSteps(), 5)
+
 if __name__ == "__main__":
     unittest.main()
